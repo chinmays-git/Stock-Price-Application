b94f0df4de75646f8e4cd438262a26c5a897d473
chinmays-git <93832217+chinmays-git@users.noreply.github.com>
2021-12-26 17:10:57 +0530

    Add files via upload

---
+++ StockApp.py
@@ -0,0 +1,37 @@
+import yfinance as yf
+import streamlit as st 
+
+
+st.write("""
+         
+# Stock Price Application 
+
+ The stock **closing price** and **volume** of Apple !
+
+""")
+
+
+#define the ticker symbol ticker for apple is "AAPL"
+tickerSymbol = 'AAPL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+
+st.write(
+    "## Closing Price :"
+)
+st.line_chart(tickerDf.Close)
+
+st.write(
+    "## Volume Price :"
+)
+st.line_chart(tickerDf.Volume)
+
+
+st.write("""
+    by Chinmay Salgaonkar
+    
+""")
